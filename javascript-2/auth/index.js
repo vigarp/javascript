9e88187e7e297a2caf5e59734b7d0131244ccda8
vigarp <vigar.8421@outlook.com>
2022-02-21 09:03:46 +0700

    add weekly-quiz branch

--- javascript-2/auth/index.js
+++
@@ -1,8 +0,0 @@
-import {generatePassword, generateEmail} from "./modules/generate.js"
-
-const data = {email: "vigar.ext@gmail.com", password: "P4ssword"};  //object
-const FormRegister = ({email, password}) => { //function destructuring object
-    return generatePassword(email, password);
-}
-
-console.log(FormRegister(data));
---
+++ quiz-week1.js
@@ -0,0 +1,11 @@
+const FazzFood = (harga, voucher, jarak, pajak) => {
+    console.log
+    let a = voucher;
+    let potongan = "";
+    if ((a ==! "FAZZFOOD5") && (harga >= 50000)) {
+        potongan = harga * 0.5;
+        return `Potongan Harg`
+    }
+    let argo = jarak * 5000;
+
+}
